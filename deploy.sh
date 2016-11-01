7805afcb5700c38bc3940e7b547fe49c81e25ed1
Brian Palmer <brian@codekitchen.net>
2016-11-01 15:01:23 -0600

    gzip deploys and don't upload sourcemaps

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
-aws s3 sync --acl public-read public/ s3://codekitchen.net
+NORMAL=$(tput sgr0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2; tput bold)
+YELLOW=$(tput setaf 3)
+
+function red() {
+    echo "$RED$*$NORMAL"
+}
+
+function green() {
+    echo "$GREEN$*$NORMAL"
+}
+
+function yellow() {
+    echo "$YELLOW$*$NORMAL"
+}
+
+SRC_DIR=public/
+DEP_DIR=deploy/
+
+red 'Setting up deploy dir'
+rm -rf $DEP_DIR
+rsync -a $SRC_DIR $DEP_DIR
+
+yellow 'Compressing js files'
+find $DEP_DIR -name '*.js' -exec sed -i '' -e '/# sourceMappingURL=/d' {} \; -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
+
+green 'Uploading html files'
+aws s3 sync --acl public-read --exclude '*.*' --include '*.html' $DEP_DIR s3://codekitchen.net
+
+green 'Uploading js files'
+aws s3 sync --acl public-read --exclude '*.*' --include '*.js' --content-type='application/javascript'  --content-encoding='gzip' --metadata-directive=REPLACE $DEP_DIR s3://codekitchen.net
+
+red 'Removing deploy dir'
+rm -rf $DEP_DIR
